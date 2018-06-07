ab47305c614cc0e3cacf76523527df9a758e0349
Rodnei <rodneicar@gmail.com>
2018-06-07 15:39:54 -0300

    Initial commit

---
+++ Aulas/Joguinho_advinhacao.py
@@ -0,0 +1,9 @@
+from random import randint
+
+numero = randint(0, 9)
+
+chute = int(input("Digite um numero de 0 a 9: "))
+if numero == chute:
+    print("Você Acertou: ")
+else:
+    print("Você errou: ")
