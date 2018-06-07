64e3960c8e37286e276d17f6a535de3ff9faa54c
Rodnei <rodneicar@gmail.com>
2018-06-07 18:27:09 -0300

    Melhorado.

--- Aulas/Joguinho_advinhacao.py
+++ Aulas/Joguinho_advinhacao.py
@@ -1,23 +1,28 @@
 from random import randint
 
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
 numero = randint(0, 9)
 print(numero)
 
-chute = int(input("Digite um numero de 0 a 9: "))
+tentativas = 3
 
-numero_certo = numero == chute
-maior = chute > numero
-menor = chute < numero
+while(tentativas > 0):
+    chute = int(input("Digite um numero de 0 a 9: "))
 
+    numero_certo = numero == chute
+    maior = chute > numero
+    menor = chute < numero
 
-
-
-if (numero_certo):
-    print("Você acertou: ")
-else:
-    if(maior):
-        print("Seu chute foi maior: ")
-    elif(menor):
-        print("Seu chute foi Menor: ")
+    if (numero_certo):
+        print("Você acertou: ")
+    else:
+        if(maior):
+            print("Seu chute foi maior: ")
+        elif(menor):
+            print("Seu chute foi Menor: ")
+    tentativas = tentativas - 1
 
 print("Final do jogo: ")
