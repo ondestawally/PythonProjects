8f1648cddc8f108ba19c12abe6ea15a82c22ee39
Rodnei <rodneicar@gmail.com>
2018-06-11 21:32:50 -0300

    Implementando o laço for.

--- Aulas/Joguinho_advinhacao.py
+++ Aulas/Joguinho_advinhacao.py
@@ -13,12 +13,17 @@ for rodadas in range(1, tentativas + 1):
     print("Tentativa {} de {} ".format(rodadas, tentativas))
     chute = int(input("Digite um numero de 0 a 9: "))
 
+    if (chute < 1 or chute > 9):
+        print("Número inválido: tente outra vez:")
+        continue
     numero_certo = numero == chute
     maior = chute > numero
     menor = chute < numero
 
+
     if (numero_certo):
         print("Você acertou: ")
+        break
     else:
         if(maior):
             print("Seu chute foi maior: ")
